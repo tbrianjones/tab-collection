fb2321c705931ec015a5e14df7a2fc2992f7a0ed
T. Brian Jones <tbrianjones@gmail.com>
2012-10-20 05:10:54 +0000

    changed header title

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 
 	<head> 
 
-		<title>My Page</title> 
+		<title>New Page Title</title> 
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
 		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
